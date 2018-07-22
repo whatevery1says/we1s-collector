453edc7ecfc3ec4e224532dfc8afc5e77c974e29
lcthomas <31972271+lcthomas@users.noreply.github.com>
2018-07-22 14:00:16 -0700

    write result_filter col to expanded query.csv

--- query_expander.py
+++ query_expander.py
@@ -11,20 +11,20 @@ def expand_querylist(fname='queries.csv', fout='queries-expanded.csv'):
     the begin_date is two years joined by ... into multiple rows
     of year queries, pass through others unchanged. For example:
 
-        source_title,source_id,keyword_string,begin_date,end_date
-        Chicago Daily Herald,163823,liberal arts,1990-01-01,1990-02-01
-        Chicago Daily Herald,163823,liberal arts,2000...2003,
-        Chicago Daily Herald,163823,liberal arts,2010-01-01,2010-02-01
+        source_title,source_id,keyword_string,begin_date,end_date,result_filter
+        Chicago Daily Herald,163823,liberal arts,1990-01-01,1990-02-01,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2000...2003,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2010-01-01,2010-02-01,liberal arts
 
     ...here the middle row is expanded to three rows:
 
-        source_title,source_id,keyword_string,begin_date,end_date
-        Chicago Daily Herald,163823,liberal arts,1990-01-01,1990-02-01
-        Chicago Daily Herald,163823,liberal arts,2000-01-01,2000-12-31
-        Chicago Daily Herald,163823,liberal arts,2001-01-01,2001-12-31
-        Chicago Daily Herald,163823,liberal arts,2002-01-01,2002-12-31
-        Chicago Daily Herald,163823,liberal arts,2002-01-01,2003-12-31
-        Chicago Daily Herald,163823,liberal arts,2010-01-01,2010-02-01
+        source_title,source_id,keyword_string,begin_date,end_date,result_filter
+        Chicago Daily Herald,163823,liberal arts,1990-01-01,1990-02-01,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2000-01-01,2000-12-31,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2001-01-01,2001-12-31,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2002-01-01,2002-12-31,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2002-01-01,2003-12-31,liberal arts
+        Chicago Daily Herald,163823,liberal arts,2010-01-01,2010-02-01,liberal arts
     """
     queryrows = []
     with open(fname, 'r') as csvfile:
@@ -35,7 +35,8 @@ def expand_querylist(fname='queries.csv', fout='queries-expanded.csv'):
                     'source_id':row['source_id'],
                     'keyword_string':row['keyword_string'],
                     'begin_date':row['begin_date'],
-                    'end_date':row['end_date']
+                    'end_date':row['end_date'],
+                    'result_filter':row['result_filter']
                     }
             if '...' in row['begin_date'] and not row['end_date']:
                 terms = [int(x) for x in row['begin_date'].split('...')]
@@ -44,13 +45,15 @@ def expand_querylist(fname='queries.csv', fout='queries-expanded.csv'):
                                       row['source_id'],
                                       row['keyword_string'],
                                       str(year)+'-01-01',
-                                      str(year)+'-12-31'])
+                                      str(year)+'-12-31',
+                                      row['result_filter']])
             else:
                 queryrows.append([row['source_title'],
                                   row['source_id'],
                                   row['keyword_string'],
                                   row['begin_date'],
-                                  row['end_date']])
+                                  row['end_date'],
+                                  row['result_filter']])
     pprint.pprint(queryrows)
     with open(fout, 'w') as outfile:
         csvwriter = csv.writer(outfile, delimiter=',')
