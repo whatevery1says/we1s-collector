3b8cf8bbb2cf4b88dcfec0d1b0ae12679d0e3a8a
Jeremy Douglass <jeremydouglass@gmail.com>
2019-10-09 14:47:50 -0700

    config.py: add oauth_id and oauth_secret handling for secrets management and local dev

--- config/config.py
+++ config/config.py
@@ -11,8 +11,14 @@ MY_PROJECT_ID = 'your-user@email.com'
 MY_USERNAME = 'your-user@wsk'
 MY_PASSWORD = 'your-password'
 
+MY_OATH_ID = 'your-oath-id'
+MY_OATH_SECRET = 'your-oath-secret'
+
 # Do not change
 LN_ENVIRONMENT = get_docker_secret('ln_environment', default=MY_ENVIRONMENT)
 LN_PROJECT_ID = get_docker_secret('ln_project_id', default=MY_PROJECT_ID)
 LN_USERNAME = get_docker_secret('ln_username', default=MY_USERNAME)
 LN_PASSWORD = get_docker_secret('ln_password', default=MY_PASSWORD)
+
+LN2_OAUTH_ID = get_docker_secret('ln2_oauth_id', default=MY_OAUTH_ID)
+LN2_OAUTH_SECRET = get_docker_secret('ln2_oauth_secret', default=MY_OAUTH_SECRET)
